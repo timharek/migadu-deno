edfcb28c7d80cc5a19bd073b54bd3e47bb12322b
Tim Hårek Andreassen <tim@harek.no>
2023-12-23 19:21:55 +0100

    chore: Clean up
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/cli.ts
+++ src/cli.ts
@@ -1,129 +1,9 @@
-import 'https://deno.land/std@0.191.0/dotenv/load.ts';
 import config from '../deno.json' assert { type: 'json' };
+import 'https://deno.land/std@0.210.0/dotenv/load.ts';
 import { Command } from '../deps.ts';
-import * as Mailbox from './cli/mailbox.ts';
-import * as Identity from './cli/identity.ts';
-import { CLI } from '../mod.ts';
+import { Mailbox } from './classes/mailbox.ts';
 
-const mailbox = new Command()
-  .description(
-    'Manage mailboxes.',
-  )
-  .alias('mbox')
-  .command('index', 'Get all mailboxes of a domain.')
-  .alias('list')
-  .action(async (options: CLI.GlobalOptions) => {
-    console.log(await Mailbox.index(options));
-  })
-  .command('show <localPart:string>', 'Get single mailboxes for a domain.')
-  .alias('get')
-  .action(async (options: CLI.GlobalOptions, localPart: string) => {
-    console.log(await Mailbox.show(options, localPart));
-  })
-  .command(
-    'create <name:string> <localPart:string> [password:string]',
-    'Create new mailbox for domain.',
-  )
-  .alias('add')
-  .option(
-    '-i, --internal',
-    'Internal, private mailbox and restrict it to receive messages only via Migadu outgoing servers.',
-  )
-  .option(
-    '-r, --recovery <inviteEmail:string>',
-    'E-mail address to receieve invite.',
-  )
-  .action(
-    async (
-      options: CLI.CreateOptions,
-      name: string,
-      local_part: string,
-      password?: string,
-    ) => {
-      console.log(
-        await Mailbox.create(options, {
-          name,
-          local_part,
-          password,
-          is_internal: options.internal,
-          password_recovery_email: options.recovery,
-        }),
-      );
-    },
-  )
-  .command('delete <localPart:string>', 'Remove mailbox from domain.')
-  .alias('remove')
-  .alias('rm')
-  .action(async (options: CLI.GlobalOptions, localPart: string) => {
-    console.log(await Mailbox.remove(options, localPart));
-  })
-  // TODO: Add optional prop for a body
-  .command('update <localPart:string>', 'Update mailbox on domain.')
-  .alias('up')
-  .action(async (options: CLI.GlobalOptions, localPart: string) => {
-    console.log(await Mailbox.updateInteractive(options, localPart));
-  });
-
-const identity = new Command()
-  .description(
-    'Manage identities on a specific mailbox.',
-  )
-  .alias('id')
-  .command(
-    'index <localPart:string>',
-    'Get all identities for mailbox on domain.',
-  )
-  .alias('list')
-  .action(async (options: CLI.GlobalOptions, localPart: string) => {
-    console.log(await Identity.index(options, localPart));
-  })
-  .command(
-    'show <localPart:string> <id:string>',
-    'Get single identity for mailbox on domain.',
-  )
-  .alias('get')
-  .action(async (options: CLI.GlobalOptions, localPart: string, id: string) => {
-    console.log(await Identity.show(options, localPart, id));
-  })
-  .command(
-    'create <mailbox:string> <name:string> <localPart:string> [password:string]',
-    'Create new identity for mailbox for domain.',
-  )
-  .alias('add')
-  .action(
-    async (
-      options: CLI.GlobalOptions,
-      mailbox: string,
-      name: string,
-      local_part: string,
-      password?: string,
-    ) => {
-      console.log(
-        await Identity.create(options, {
-          mailbox,
-          name,
-          local_part,
-          password,
-        }),
-      );
-    },
-  )
-  .command(
-    'delete <mailbox:string> <id:string>',
-    'Remove identity for mailbox on domain.',
-  )
-  .alias('remove')
-  .alias('rm')
-  .action(async (options: CLI.GlobalOptions, mailbox: string, id: string) => {
-    console.log(await Identity.remove(options, mailbox, id));
-  })
-  .command('update <mailbox:string> <id:string>', 'Update mailbox on domain.')
-  .alias('up')
-  .action(() => {
-    console.log('Not implemented yet');
-  });
-
-await new Command()
+const app = new Command()
   .name(config.name)
   .version(config.version)
   .description('CLI for Migadu official API.')
@@ -133,24 +13,126 @@ await new Command()
     'Example #1',
     'magic -v',
   )
+  .action(function (): void {
+    this.showHelp();
+  })
   .globalEnv(
     'MIGADU_USER=<email:string>',
     'E-mail address for Migadu account (not mailbox email).',
+    { prefix: 'MIGADU_', required: true },
   )
-  .globalEnv('USER_TOKEN=<token:string>', 'API token for Migadu account.')
-  .globalEnv('DOMAIN=<value:string>', 'A domain in the Migadu account.')
+  .globalEnv(
+    'MIGADU_USER_TOKEN=<token:string>',
+    'API token for Migadu account.',
+    { prefix: 'MIGADU_', required: true },
+  )
+  .globalEnv(
+    'MIGADU_DOMAIN=<domain:string>',
+    'A domain in the Migadu account.',
+    { prefix: 'MIGADU_', required: true },
+  )
   .globalOption(
-    '--migaduUser [email:string]',
+    '--user <email:string>',
     'E-mail address for Migadu account (not mailbox email).',
   )
-  .globalOption('--userToken [token:string]', 'API token for Migadu account.')
-  .globalOption('--domain [domain:string]', 'A domain in the Migadu account.')
-  .globalOption('-v, --verbose', 'A more verbose output.', {
-    collect: true,
-    value: (value: boolean, previous = 0) => (value ? previous + 1 : 0),
+  .globalOption('--user-token <token:string>', 'API token for Migadu account.')
+  .globalOption('--domain <domain:string>', 'A domain in the Migadu account.')
+  .globalOption('--json', 'JSON output.');
+
+type GlobalOptions = typeof app extends
+  Command<void, void, void, [], infer Options extends Record<string, unknown>>
+  ? Options
+  : never;
+
+const mailbox = new Command<GlobalOptions>()
+  .description('Manage mailboxes.')
+  .alias('mbox')
+  .command('index', 'Get all mailboxes of a domain.')
+  .alias('list')
+  .action(async ({ domain, json }) => {
+    const result = await Mailbox.list(domain);
+    if (json) {
+      console.log(JSON.stringify(result, null, 2));
+      return;
+    }
+    console.log(
+      result.map((mbox) => `${mbox.name} <${mbox.email}>`).join('\n'),
+    );
   })
-  .globalOption('--json', 'JSON output.')
-  .globalOption('-d, --debug', 'Debug output.')
-  .command('mailbox', mailbox)
-  .command('identity', identity)
-  .parse(Deno.args);
+  .command('show <localPart:string>', 'Get single mailboxes for a domain.')
+  .alias('get')
+  .action(async ({ domain, json }, localPart) => {
+    const mbox = await Mailbox.get(domain, localPart);
+    if (json) {
+      console.log(JSON.stringify(mbox, null, 2));
+      return;
+    }
+    console.log(`${mbox.name} <${mbox.email}>`);
+  })
+  .command(
+    'create <name:string> <localPart:string> [password:string]',
+    'Create new mailbox for domain.',
+  )
+  .alias('add')
+  .option(
+    '-r, --recovery-email <inviteEmail:string>',
+    'E-mail address to receieve invite.',
+  )
+  .action(
+    async function (
+      { domain, json, recoveryEmail },
+      name,
+      local_part,
+      password,
+    ): Promise<void> {
+      let mbox;
+      if (!password || !recoveryEmail) {
+        this.showHelp();
+      }
+      if (password) {
+        mbox = await Mailbox.create({
+          domain,
+          name,
+          local_part,
+          password_method: 'password',
+          password,
+        });
+      }
+      if (!password && recoveryEmail) {
+        mbox = await Mailbox.create({
+          domain,
+          name,
+          local_part,
+          password_method: 'invitation',
+          password_recovery_email: recoveryEmail,
+        });
+      }
+      if (!mbox) {
+        this.showHelp();
+        Deno.exit(0);
+      }
+      if (json) {
+        console.log(JSON.stringify(mbox, null, 2));
+        return;
+      }
+      console.log(`Created ${mbox.name} <${mbox.email}>`);
+    },
+  )
+  .command('delete <localPart:string>', 'Remove mailbox from domain.')
+  .alias('remove')
+  .alias('rm')
+  .action(async ({ domain }, localPart) => {
+    console.log(await Mailbox.delete(domain, localPart));
+  })
+  // TODO: Add optional prop for a body
+  .command('update <localPart:string>', 'Update mailbox on domain.')
+  .alias('up')
+  .action((_options, _localPart) => {
+    console.log('Not implemented yet.');
+  });
+
+if (import.meta.main) {
+  await app
+    .command('mailbox', mailbox)
+    .parse(Deno.args);
+}
