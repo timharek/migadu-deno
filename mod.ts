c9b0a14786a9f1f1fcd0ac9eed88febc4b5197c0
Tim Hårek Andreassen <tim@harek.no>
2023-12-23 22:57:12 +0100

    chore: Update module
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- mod.ts
+++ mod.ts
@@ -1,28 +1,26 @@
 /**
- * A module that can access [Migadu's API](https://www.migadu.com/api/).
+ * A module that can interact with [Migadu's API](https://www.migadu.com/api/).
  *
  * ## Usage
+ * Remember to set the environment variables:
+ *
+ * - `MIGADU_USER`
+ * - `MIGADU_USER_TOKEN`
+ * - `MIGADU_DOMAIN`
  *
  * ### List all mailboxes
  *
+ * @example
  * ```typescript
- * import { index, Migadu } from "https://deno.land/x/migadu/mod.ts";
+ * import { Mailbox } from './src/classes/mailbox.ts';
  *
- * const mailboxes = await index<Migadu.Mailbox>({
- *   domain: "example.org",
- *   migaduUser: "alice@example.org",
- *   userToken: "abcxyz",
- *   type: "mailbox",
- * });
+ * const mailboxes = await Mailbox.list("example.org")
  *
- * console.log(mailboxes);
+ * console.log(mailboxes)
  * ```
  *
  * @module
  */
 
-export * from './src/migadu.ts';
-export * from './src/types/migadu.d.ts';
-
-export type { CLI, OptionProps } from './src/types/index.d.ts';
-export type { Migadu } from './src/types/migadu.d.ts';
+export { Mailbox } from './src/classes/mailbox.ts';
+export { Identity } from './src/classes/identity.ts';
